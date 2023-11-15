56551ec9ba997c545beb393c69331ec56a8c601d
altmktgdev <143544217+altmktgdev@users.noreply.github.com>
2023-11-15 12:07:56 -0800

    top widget included

--- core-functions/top_widget-styles.php
+++ core-functions/top_widget-styles.php
@@ -1,7 +1,7 @@
 
 <?php
 
-// function my_theme_enqueue_styles() {
-//     wp_enqueue_style('my-custom-style', get_theme_file_uri('widgets.css'), array(), '1.0.0');
-// }
-// add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+function my_theme_enqueue_styles() {
+    wp_enqueue_style('my-custom-style', get_theme_file_uri('widgets.css'), array(), '1.0.0');
+}
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
