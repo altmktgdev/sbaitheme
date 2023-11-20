44ff1a8e67b8f2765d3cf87dc9d232db8eb0c370
altmktgdev <143544217+altmktgdev@users.noreply.github.com>
2023-11-20 13:04:12 -0800

    new adjustmens

--- category.php
+++ category.php
@@ -6,27 +6,22 @@
  */
 
 get_header(); ?>
-
 <div class="content-info-column">
 	<div class="content-info-column__item">
-		<?php
-		// Obtener todas las categorías de WordPress
-		$categories = get_categories();
-
-		// Recorrer las categorías y mostrar el shortcode para cada una
-		foreach ($categories as $category) {
-			$category_name = $category->slug; // Obtener el nombre de la categoría
-			$category_title = $category->name; // Obtener el título de la categoría
-			echo do_shortcode("[main_mid_slider top='9' tag='' category='$category_name' title='$category_title']");
-		}
-		?>
+		<?php echo do_shortcode('[main_mid_slider top="9" tag="" category="News" title="News"]'); ?>
+		<?php echo do_shortcode('[main_mid_slider top="9" tag="" category="NFL" title="NFL"]'); ?>
+		<?php echo do_shortcode('[main_mid_slider top="9" tag="" category="NBA" title="NBA"]'); ?>
+		<?php echo do_shortcode('[main_mid_slider top="9" tag="" category="MLB" title="MLB"]'); ?>
+		<?php echo do_shortcode('[main_mid_slider top="9" tag="" category="NHL" title="NHL"]'); ?>
+		<?php echo do_shortcode('[main_mid_slider top="9" tag="" category="UFC" title="UFC"]'); ?>
+		<?php echo do_shortcode('[main_mid_slider top="9" tag="" category="Champions League" title="Champions League"]'); ?>
+		<?php echo do_shortcode('[main_mid_slider top="9" tag="" category="NCAAF" title="NCAAF"]'); ?>
 	</div>
 	<div class="content-info-column__item">
 		<div class="content-info-column__sidebar-container">
-			<?php dynamic_sidebar('aside-widget'); ?>
+		<?php dynamic_sidebar( 'aside-widget' );?>
 			<?php echo do_shortcode('[sd-widgets widget="nextGames" sport="baseball" league="mlb" class="side-widget-1"]'); ?>
 		</div>
 	</div>
 </div>
-
 <?php get_footer(); ?>
