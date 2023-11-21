1064845fd73a1de60c8dc4894b9b43a3656f0b59
altmktgdev <143544217+altmktgdev@users.noreply.github.com>
2023-11-21 12:57:10 -0800

    new adjustmens

--- js/core-modules/menuMultiNivel.js
+++ js/core-modules/menuMultiNivel.js
@@ -90,6 +90,10 @@ const menuMultiNivel = () => {
 	const pushMenuAnchors = document.querySelectorAll('.push-menu__anchor');
 	pushMenuAnchors.forEach(anchor => {
 		anchor.addEventListener('click', (event) => {
+			if (anchor.textContent.trim().toLowerCase().includes('see all')) {
+                // No se ejecuta preventDefault para permitir el comportamiento normal del enlace
+                return;
+            }
 			event.preventDefault();
 			// Verifica si el ancho del viewport es mayor o igual a 1280px
 			if (window.innerWidth >= 1280) {
