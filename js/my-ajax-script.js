d696cace6b733ba533aa114841ca2a2f87ff6372
altmktgdev <143544217+altmktgdev@users.noreply.github.com>
2023-11-21 07:20:46 -0800

    new adjustmens

--- js/my-ajax-script.js
+++ js/my-ajax-script.js
@@ -1,24 +1,21 @@
-let pageNumber = 1;  // Empezamos en la página 1 por defecto
+jQuery(document).ready(function($) {
+    var page = 2; // Establece la página inicial para cargar más publicaciones
 
-jQuery(document).on('click', '.news-show-more__button-expand', function() {
-    pageNumber++; // Incrementamos el número de página
+    $('.news-show-more__button-expand').on('click', function() {
+        var data = {
+            'action': 'load_more_posts',
+            'page_number': page,
+            'category': my_ajax_object.category // Asegúrate de pasar la categoría correcta aquí
+        };
 
-    jQuery.ajax({
-        type: 'POST',
-        url: my_ajax_object.ajax_url,
-        data: {
-            action: 'load_more_posts',
-            category: window.location.pathname.split('/')[2], // Obtenemos la categoría de la URL
-            page_number: pageNumber // Pasamos el número de página actualizado
-        },
-        success: function(response) {
-            // Aquí agregas el nuevo contenido a tu contenedor
-            // Por ejemplo:
-            jQuery('.news-show-more__container').append(response);
-            return false;
-        }
+        $.post(my_ajax_object.ajax_url, data, function(response) {
+            $('.news-show-more__container').append(response);
+            page++;
+
+            // Si no hay más publicaciones, oculta el botón
+            if (response === '') {
+                $('.news-show-more__button-expand').hide();
+            }
+        });
     });
 });
-
-
-
