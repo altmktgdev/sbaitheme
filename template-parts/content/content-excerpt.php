dafc6cfaba558f3802b779ea24beb7b1f986a7f0
altmktgdev <143544217+altmktgdev@users.noreply.github.com>
2023-11-22 05:19:21 -0800

    new adjustmens

--- template-parts/content/content-excerpt.php
+++ template-parts/content/content-excerpt.php
@@ -12,14 +12,28 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="title-div">
+        <header class="entry-header">
+            <h2 class="entry-title default-max-width"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        </header>
+    </div>
 
-	<?php get_template_part( 'template-parts/header/excerpt-header', get_post_format() ); ?>
-
-	<div class="entry-content">
-		<?php get_template_part( 'template-parts/excerpt/excerpt', get_post_format() ); ?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer default-max-width">
-		<?php twenty_twenty_one_entry_meta_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-${ID} -->
+    <div class="image-excerpt-div">
+        <div class="featured-image-div">
+            <?php
+            if (has_post_thumbnail()) {
+                $featured_image = get_the_post_thumbnail_url(null, 'full');
+                echo '<img src="' . esc_url($featured_image) . '" class="custom-featured-image" alt="' . esc_attr(get_the_title()) . '">';
+            }
+            ?>
+        </div>
+        <div class="excerpt-div">
+            <div class="entry-content">
+                <?php get_template_part( 'template-parts/excerpt/excerpt', get_post_format() ); ?>
+            </div><!-- .entry-content -->
+            <footer class="entry-footer default-max-width">
+                <?php twenty_twenty_one_entry_meta_footer(); ?>
+            </footer><!-- .entry-footer -->
+        </div>
+    </div>
+</article><!-- #post-<?php the_ID(); ?> -->
