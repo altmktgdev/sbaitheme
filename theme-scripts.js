24fd9d34ba363e6e5e562854c7b9cb40fb481e7a
altmktgdev <143544217+altmktgdev@users.noreply.github.com>
2023-11-22 10:35:08 -0800

    new adjustmens

--- theme-scripts.js
+++ theme-scripts.js
@@ -102,12 +102,6 @@ var menuMultiNivel = function menuMultiNivel() {
   var pushMenuAnchors = document.querySelectorAll('.push-menu__anchor');
   pushMenuAnchors.forEach(function (anchor) {
     anchor.addEventListener('click', function (event) {
-      if (anchor.textContent.trim().toLowerCase().includes('see all')) {
-        // No se ejecuta preventDefault para permitir el comportamiento normal del enlace
-        return;
-      }
-      event.preventDefault();
-      // Verifica si el ancho del viewport es mayor o igual a 1280px
       if (window.innerWidth >= 1280) {
         // Previene la acción por defecto del enlace
         event.preventDefault();
