50a3f3ef8acdb6e562049959b8eabaa5660baef6
altmktgdev <143544217+altmktgdev@users.noreply.github.com>
2023-11-30 09:07:12 -0800

    new adjustmens

--- core-functions/jquery.php
+++ core-functions/jquery.php
@@ -1,9 +1,9 @@
 <?php
 function modify_jquery_and_optimize_scripts() {
     if (!is_admin()) {
-        // Optimiza jQuery
+        // Cargar la versión "slim" de jQuery asincrónicamente
         wp_deregister_script('jquery');
-        wp_register_script('jquery', includes_url('/js/jquery/jquery.js'), false, NULL, true);
+        wp_register_script('jquery', 'https://code.jquery.com/jquery-3.6.0.slim.min.js', false, '3.6.0', true);
         wp_enqueue_script('jquery');
 
         // Ejemplo para desencolar un script no utilizado en la homepage
