5f692c9d5cda7f320418a89c7e3d7a1c021cf1d4
altmktgdev <143544217+altmktgdev@users.noreply.github.com>
2023-11-30 07:01:36 -0800

    new adjustmens

--- core-functions/jquery.php
+++ core-functions/jquery.php
@@ -1,12 +1,15 @@
 <?php
-function modify_jquery() {
+function modify_jquery_and_optimize_scripts() {
     if (!is_admin()) {
-        // Deregister default jQuery
+        // Optimiza jQuery
         wp_deregister_script('jquery');
-        
-        // Register the script again, but with defer or async attribute
         wp_register_script('jquery', includes_url('/js/jquery/jquery.js'), false, NULL, true);
         wp_enqueue_script('jquery');
+
+        // Ejemplo para desencolar un script no utilizado en la homepage
+        if (is_front_page() || is_home()) {
+            wp_dequeue_script('unneeded-script-handle');
+        }
     }
 }
-add_action('wp_enqueue_scripts', 'modify_jquery');
+add_action('wp_enqueue_scripts', 'modify_jquery_and_optimize_scripts');
--- core-functions/top_widget-styles.php
+++ core-functions/top_widget-styles.php
@@ -1,17 +1,16 @@
 
 <?php
+function modify_jquery_and_optimize_scripts() {
+    if (!is_admin()) {
+        // Optimiza jQuery
+        wp_deregister_script('jquery');
+        wp_register_script('jquery', includes_url('/js/jquery/jquery.js'), false, NULL, true);
+        wp_enqueue_script('jquery');
 
-function my_theme_enqueue_styles() {
-    wp_enqueue_style('my-custom-style', get_theme_file_uri('widgets.css'), array(), '1.0.2', false);
-
-    // Agrega un filtro para añadir el atributo 'defer' al tag de CSS
-    add_filter('style_loader_tag', 'add_async_attribute_to_css', 10, 2);
-}
-add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
-
-function add_async_attribute_to_css($html, $handle) {
-    if ('my-custom-style' === $handle) {
-        return str_replace("rel='stylesheet'", "rel='preload' as='style' onload=\"this.onload=null;this.rel='stylesheet'\"", $html);
+        // Ejemplo para desencolar un script no utilizado en la homepage
+        if (is_front_page() || is_home()) {
+            wp_dequeue_script('unneeded-script-handle');
+        }
     }
-    return $html;
 }
+add_action('wp_enqueue_scripts', 'modify_jquery_and_optimize_scripts');
