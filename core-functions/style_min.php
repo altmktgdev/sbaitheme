9ae792a1c03ee54f0ce9903de30558c2f64d78dd
altmktgdev <143544217+altmktgdev@users.noreply.github.com>
2023-11-30 09:46:24 -0800

    new adjustmens

--- core-functions/style_min.php
+++ core-functions/style_min.php
@@ -1,5 +1,5 @@
 <?php
-function remove_block_library_styles() {
-    wp_dequeue_style('wp-block-library'); // Desencola el archivo de estilos de la librería de bloques
-}
-add_action('wp_enqueue_scripts', 'remove_block_library_styles', 100);
+// function remove_block_library_styles() {
+//     wp_dequeue_style('wp-block-library'); // Desencola el archivo de estilos de la librería de bloques
+// }
+// add_action('wp_enqueue_scripts', 'remove_block_library_styles', 100);
